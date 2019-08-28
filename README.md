333a1a6d11b6502e19a30ab5b561cec0a0f5440e
tangleiNUAA <tanglei_0312@163.com>
2019-08-29 03:43:01 +0800

    [init] Init this project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ci-demo
+
+ci-demo is a just a demo for Jenkins ci. This project describe how to build and deploy a project using Jenkins continue integration.
---
+++ src/main/java/org/vidi/ci/cidemo/CiDemoApplication.java
@@ -0,0 +1,13 @@
+package org.vidi.ci.cidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiDemoApplication.class, args);
+    }
+
+}
